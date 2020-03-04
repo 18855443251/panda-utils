4852822ead6fb069b133a9d97795e83a3b654395
ewall <1054064180@qq.com>
2020-03-04 17:22:34 +0800

    feat：init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+
+module.exports = {
+  mode: "production",
+
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "panda-utils.js",
+    library: "pandaUtils",
+    libraryTarget: "umd"
+  },
+
+  devtool: "cheap-module-source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: "usage",
+                  corejs: { version: 3, proposals: true }
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
